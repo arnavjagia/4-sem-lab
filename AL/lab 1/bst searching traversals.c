9b8cf09471df02f3ddc8fd2c25739d585566a9b6
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-09 16:14:29 +0530

    Update bst searching traversals.c
    added sample output

--- AL/lab 1/bst searching traversals.c
+++ AL/lab 1/bst searching traversals.c
@@ -105,3 +105,14 @@ int main() {
     inorder(root);
     return 0;
 }
+
+/*
+Preorder
+1 2 3 4 5 6 7 8 9 10 
+Postorder
+10 9 8 7 6 5 4 3 2 1 
+Inorder
+1 2 3 4 5 6 7 8 9 10 
+Searching for 10
+1 2 3 4 5 6 7 8 9 10 12
+*/
