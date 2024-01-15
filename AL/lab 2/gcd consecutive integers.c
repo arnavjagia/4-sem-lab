8b5910fa803c97f7779878e55d3da31cc6c8c6f3
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-15 23:40:48 +0530

    Create gcd consecutive integers.c

---
+++ AL/lab 2/gcd consecutive integers.c
@@ -0,0 +1,8 @@
+/*
+Write a program to find GCD using consecutive integer checking method and analyze its time efficiency.
+*/
+#include <stdio.h>
+int gcd(int a, int b)
+{
+    for (int i = 3; i < 
+}
