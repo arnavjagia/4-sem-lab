456e5f9beedcdba819d5d03b110c2b39f6580a5e
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-22 10:18:46 +0530

    Create bfs.py

---
+++ AIL/lab 4/bfs.py
@@ -0,0 +1,43 @@
+"""
+"""
+
+class Dequeue:
+    def __init__(self):
+        self.arr = []
+    def enqueue(self, ele):
+        self.arr.append(ele)
+    def dequeue(self):
+        return self.arr.pop(0)
+    def __str__(self):
+        res = "Queue: "
+        for ele in self.arr:
+            res += str(ele) + " "
+        return res
+
+class Graph:
+    def __str__(self):
+        res = ""
+        for tail, headlist in self.adj.items():
+            for head in headlist:
+                res += f"({tail} -> {head})\t"
+            res += "\n"
+        return res
+    def __init__(self):
+        self.adj = {}
+    def add_edge(self, tail, head):
+        if tail not in self.adj:
+            self.adj[tail] = []
+        self.adj[tail].append(head)                    
+
+if __name__ == "__main__":
+    d = Dequeue()
+    print(d)
+    d.enqueue(1)
+    print(d)
+    d.enqueue(2)
+    print(d)
+    g = Graph()
+    g.add_edge(1,2)
+    g.add_edge(1,3)
+    g.add_edge(2,3)
+    print(g)
