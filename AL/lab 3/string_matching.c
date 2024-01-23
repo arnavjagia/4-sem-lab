e17936f1f5167bf4c8f9c20e8830bc9aa342b093
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-23 15:09:07 +0530

    Update string_matching.c

--- AL/lab 3/string_matching.c
+++ AL/lab 3/string_matching.c
@@ -6,7 +6,10 @@ Write a program to implement brute-force string matching. Analyze its time effic
 
 int main()
 {
-    
+    char str1[] = "Hellooo";
+    char str2[] = "World";
+    printf("\n%d", sizeof(str1));
+    printf("\n%d", sizeof(str2));
     
     return 0;
 }
