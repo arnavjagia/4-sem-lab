4aff796861880683dc7ec74600c6415675398176
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-23 15:40:57 +0530

    Update string_matching.c

--- AL/lab 3/string_matching.c
+++ AL/lab 3/string_matching.c
@@ -8,15 +8,15 @@ int string_match(char *str, char *substr)
 {
     int len1 = 0, len2 = 0;
 
-    while(str[len1] != '\0') {len1++;} len1++;
-    while(substr[len2] != '\0') {len2++;} len2++;
+    while(str[len1++] != '\0'); len1--;
+    while(substr[len2++] != '\0'); len2--;
     printf("\n%d", len1);
     printf("\n%d", len2);
 }
 
 int main()
 {
-    char str[] = "Hellooo";
+    char str[] = " ";
     char substr[] = "World";
 
     string_match(str, substr);
