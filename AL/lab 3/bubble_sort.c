6cc7bd402d035866749d26506b9758b38e370fdf
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-23 14:34:29 +0530

    Update bubble_sort.c

--- AL/lab 3/bubble_sort.c
+++ AL/lab 3/bubble_sort.c
@@ -6,8 +6,19 @@ the best and worst case.
 
 #include <stdio.h>
 
+void bubble_sort(int *arr, unsigned int len)
+{
+    for (int i=0; i<n-1; i++)
+    {
+        printf("\n%d", arr[i]);
+    }
+}
+
 int main() 
 {
+    int setofintegers[] = {56,87,34,79,23,94};
+    unsigned int len = sizeof(setofintegers)/sizeof(setofintegers[0]);
+    bubble_sort(setofintegers, len);
     return 0;
 }
 
