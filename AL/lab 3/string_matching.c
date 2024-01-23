4f30087527843e943ac1373e380c2f4e2f170eaa
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-23 15:21:00 +0530

    Update string_matching.c

--- AL/lab 3/string_matching.c
+++ AL/lab 3/string_matching.c
@@ -4,12 +4,14 @@ Write a program to implement brute-force string matching. Analyze its time effic
 
 #include <stdio.h>
 
+
+
 int main()
 {
-    char str1[] = "Hellooo";
-    char str2[] = "World";
-    printf("\n%ld", sizeof(str1));
-    printf("\n%ld", sizeof(str2));
+    char str[] = "Hellooo";
+    char substr[] = "World";
+    printf("\n%ld", sizeof(str));
+    printf("\n%ld", sizeof(substr));
     
     return 0;
 }
