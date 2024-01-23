2d2d90ecb1604949f6f96f64269aed80de6b3e32
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-23 15:33:55 +0530

    Update string_matching.c

--- AL/lab 3/string_matching.c
+++ AL/lab 3/string_matching.c
@@ -7,20 +7,17 @@ Write a program to implement brute-force string matching. Analyze its time effic
 int string_match(char *str, char *substr)
 {
     int len1 = 0, len2 = 0;
-    do {static int i=0; len1++} while (str[i] != '\0'); 
-    do {static int i=0; len2++} while (substr[i] != '\0'); 
-    printf("\n%ld", len1);
-    printf("\n%ld", len2);
-    // printf("\n%ld", sizeof(str));
-    // printf("\n%ld", sizeof(substr));
+
+    while(str[len1] != '\0') {len1++;} len1++;
+    while(substr[len2] != '\0') {len2++;} len2++;
+    printf("\n%d", len1);
+    printf("\n%d", len2);
 }
 
 int main()
 {
     char str[] = "Hellooo";
     char substr[] = "World";
-    printf("\n%ld", sizeof(str));
-    printf("\n%ld", sizeof(substr));
 
     string_match(str, substr);
     
