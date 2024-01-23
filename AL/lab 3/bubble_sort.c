2cecff1e42aa9e1ed3c169652a55a1834840d71f
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-23 14:46:46 +0530

    Update bubble_sort.c

--- AL/lab 3/bubble_sort.c
+++ AL/lab 3/bubble_sort.c
@@ -14,7 +14,7 @@ void swap (int *a, int *b)
 void bubble_sort(int *arr, unsigned int len)
 {
     for (int i=0; i<len-1; i++)
-        for (int j=i; j<len-1; j++)
+        for (int j=0; j<len-1; j++)
             if (arr[j+1]<arr[j])
                 swap(&arr[j+1], &arr[j]);
 }
