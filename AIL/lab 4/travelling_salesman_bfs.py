8c564614dc1f9210f5de75c9a838fcd3f50814b8
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-29 02:42:32 +0530

    Update travelling_salesman_bfs.py

--- AIL/lab 4/travelling_salesman_bfs.py
+++ AIL/lab 4/travelling_salesman_bfs.py
@@ -25,30 +25,41 @@ class Deque:
             res += str(ele) + " "
         return res
 
-def bfs(graph, start):
-    visited = set()
-    queue = Deque()
-    queue.enqueue((start, [start]))
+class Graph:
+    def __init__(self, adjlist : dict = {}):
+        self.adj = adjlist
 
-    while queue.arr:
-        (node, path) = queue.dequeue()
-        for next_node in set(graph[node]) - set(path):
-            if len(path) + 1 == len(graph):
-                yield path + [next_node]
-            else:
-                queue.enqueue((next_node, path + [next_node]))
+    def __str__(self):
+        res = ""
+        for tail, headlist in self.adj.items():
+            for head in headlist:
+                res += f"({tail} -> {head}, {headlist[head]})\t"
+            res += "\n"
+        return res
+    
+    def bfs(self, start):
+        queue = Deque()
+        queue.enqueue((start, [start]))
 
-def tsp_bfs(graph, start):
-    shortest_path = None
-    shortest_path_length = float('inf')
+        while queue.arr:
+            (node, path) = queue.dequeue()
+            for next_node in set(graph[node]) - set(path):
+                if len(path) + 1 == len(graph):
+                    yield path + [next_node]
+                else:
+                    queue.enqueue((next_node, path + [next_node]))
 
-    for path in bfs(graph, start):
-        path_length = sum(graph[path[i-1]][path[i]] for i in range(len(path)))
-        if path_length < shortest_path_length:
-            shortest_path = path
-            shortest_path_length = path_length
+    def tsp_bfs(self, start):
+        shortest_path = None
+        shortest_path_length = float('inf')
 
-    return shortest_path, shortest_path_length
+        for path in self.bfs(start):
+            path_length = sum(graph[path[i-1]][path[i]] for i in range(len(path)))
+            if path_length < shortest_path_length:
+                shortest_path = path
+                shortest_path_length = path_length
+
+        return shortest_path, shortest_path_length
 
 # example usage:
 graph = {
@@ -57,7 +68,9 @@ graph = {
     'C': {'A': 3, 'B': 4, 'D': 3},
     'D': {'A': 1, 'B': 2, 'C': 3}
 }
-print(tsp_bfs(graph, 'C'))
+g = Graph(graph)
+print(g.tsp_bfs('C'))
+
 
 """
 (['C', 'B', 'A', 'D'], 10)
