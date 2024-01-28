1243824d9b5f834c6c05ee2d062a00e4d70f5b62
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-01-29 00:55:42 +0530

    Create detect_cycle_bfs.py

---
+++ AIL/lab 4/detect_cycle_bfs.py
@@ -0,0 +1,87 @@
+"""
+Consider the following directed graph for detecting cycles in the graph using BFS 
+algorithm using Python.
+"""
+
+class Deque:
+    def __init__(self):
+        self.arr = []
+        
+    def enqueue(self, ele):
+        self.arr.append(ele)
+        
+    def dequeue(self):
+        return self.arr.pop(0)
+        
+    def __str__(self):
+        res = "Queue: "
+        for ele in self.arr:
+            res += str(ele) + " "
+        return res
+
+class Graph:
+    def __init__(self):
+        self.adj = {}
+
+    def __str__(self):
+        res = ""
+        for tail, headlist in self.adj.items():
+            for head in headlist:
+                res += f"({tail} -> {head})\t"
+            res += "\n"
+        return res
+        
+    def add_edge(self, tail, head):
+        if tail not in self.adj:
+            self.adj[tail] = []
+        self.adj[tail].append(head)
+
+        if head not in self.adj:
+            self.adj[head] = []
+
+    def detect_cycle(self) -> bool:
+        d = Deque()
+        visited = set()
+        indegree = {vertex : 0 for vertex in self.adj}
+
+        # maintaining indegree dict
+        for tail, headlist in self.adj.items():
+            for head in headlist:
+                indegree[head] += 1
+
+        # indegree 0 add to queue
+        for vertex in indegree:
+            if indegree[vertex] == 0:
+                d.enqueue(vertex)
+                visited.add(vertex)
+
+        return True if len(d.arr) == 0
+
+        # applying bfs
+        while len(d.arr):
+            vertex = d.dequeue()
+            # T.append(vertex)
+            for head in self.adj[vertex]:
+                if head in visited:
+                    return True
+                else:
+                    indegree[head] -= 1
+                    if indegree[head] == 0:
+                        d.enqueue(head)
+                        visited.add(head)
+        return False
+
+if __name__ == "__main__":
+    d = Dequeue()
+    print(d)
+    d.enqueue(1)
+    print(d)
+    d.enqueue(2)
+    print(d)
+    g = Graph()
+    g.add_edge(1,2)
+    g.add_edge(1,3)
+    g.add_edge(2,3)
+    print(g)
+    print(g.detect_cycle())
+    print(g.visited)
