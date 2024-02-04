077a35564f1e8160760bbaca173fb1d42401d650
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-02-05 03:26:57 +0530

    Update Uniform_cost_search.py

--- AIL/lab 5 - UCS/Uniform_cost_search.py
+++ AIL/lab 5 - UCS/Uniform_cost_search.py
@@ -3,6 +3,8 @@ Uniform Cost Search algorithm
 https://www.baeldung.com/cs/uniform-cost-search-vs-best-first-search
 https://www.javatpoint.com/ai-uninformed-search-algorithms
 https://stackoverflow.com/questions/407734/a-generic-priority-queue-for-python
+https://github.com/TheAlgorithms/Python/blob/master/graphs/dijkstra_algorithm.py
+https://www.baeldung.com/cs/uniform-cost-search-vs-dijkstras
 
 W YouTube video
 https://youtu.be/dRMvK76xQJI?si=8W7SFb4bqYE_gdpv
