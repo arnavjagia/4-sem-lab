c6d56fc648e81fe4c12910d400a53c73a9756656
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-02-05 10:13:53 +0530

    Update A* algorithm.py

--- AIL/lab 7 - A*/A* algorithm.py
+++ AIL/lab 7 - A*/A* algorithm.py
@@ -5,3 +5,25 @@ https://www.baeldung.com/cs/a-star-algorithm
 class PriorityQueue:
     def __init__(self):
         self.queue = []
+
+    def insert(self, priority, item):
+        self.queue.append((priority, item))
+        self.queue.sort(reverse = True)
+
+    def extract_min(self):
+        if not self.queue:
+            return None
+        return self.queue.pop()[1]
+    
+    def is_empty(self):
+        return False if self.queue else return True
+
+class Graph:
+    def __init__(self, adjlist : dict = {}, heuristic : dict = {}):
+        self.adj = adjlist
+        self.heuristic = heuristic
+
+    def neighbors(self, node):
+        return self.adj[node]
+        
+    # def a_star(self, )
