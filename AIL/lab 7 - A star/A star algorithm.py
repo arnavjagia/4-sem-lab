be1cda2bcdcada6346b2b7bb464112f05f5c1674
Arnav Jagia <arnavjagia04@gmail.com>
2024-02-12 01:21:34 +0530

    A star manual example

--- AIL/lab 7 - A star/A star algorithm.py
+++ AIL/lab 7 - A star/A star algorithm.py
@@ -10,23 +10,80 @@ class PriorityQueue:
         self.queue.append((priority, item))
         self.queue.sort(reverse = True)
 
-    def extract_min(self):
+    def extract_min(self) -> tuple[int, int]:
         if not self.queue:
             return None
-        return self.queue.pop()[1]
+        return self.queue.pop()
     
     def is_empty(self):
-        return False if self.queue else return True
+        return len(self.queue) == 0
 
 class Graph:
     def __init__(self, adjlist : dict = {}, heuristic : dict = {}):
         self.adj = adjlist
-        self.heuristic = heuristic
+        self.heuristics = heuristic
 
-    def neighbors(self, node):
-        return self.adj[node]
-        
-    def a_star(self):
+    def reconstruct_path(self, start, goal, prev):
+        path = []
+        current = goal
+        while current != start:
+            path = [current] + path
+            current = prev[current]
+        return [start] + path
+
+    def heuristic(self, node):
+        return self.heuristics[node]
+
+    def a_star(self, start, goal):
+        dist = {v: float('inf') for v in self.adj} # initial distance estimates
         frontier = PriorityQueue()
-        
-        
+
+        dist[start] = 0
+        frontier.insert(0, start) # "frontier" to be explored 
+        prev = {v: None for v in self.adj} # for storing lowest cost parents
+
+        closed = set()
+
+        while frontier:
+            priority_current, current = frontier.extract_min()
+            
+            if current == goal:
+                break
+
+            if current in closed:
+                continue
+            closed.add(current)
+
+            for neighbor, edge_weight in self.adj[current]:
+                alt_dist = dist[current] + edge_weight
+                if alt_dist < dist[neighbor]:
+                    dist[neighbor] = alt_dist
+                    prev[neighbor] = current
+
+                    # this is the only thing that's different from dijkstra's:
+                    priority = alt_dist + self.heuristic(neighbor)
+                    frontier.insert(priority, neighbor)
+
+        return self.reconstruct_path(start, goal, prev)
+
+if __name__ == "__main__":
+    h_dist = {
+        'A': 11,
+        'B': 6,
+        'C': 99,
+        'D': 1,
+        'E': 7,
+        'G': 0,
+    }
+
+    Graph_nodes = {
+        'A': [('B', 2), ('E', 3)],
+        'B': [('A', 2), ('C', 1), ('G', 9)],
+        'C': [('B', 1)],
+        'D': [('E', 6), ('G', 1)],
+        'E': [('A', 3), ('D', 6)],
+        'G': [('B', 9), ('D', 1)]
+    }
+
+    graph1 = Graph(Graph_nodes, h_dist)
+    print(graph1.a_star('A', 'G')) # ['A', 'E', 'D', 'G']
