7afaff582b32055214b031da0cc9467b87fe96c6
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-02-27 11:20:46 +0530

    the same

--- AL/al-midsem-practice/mergesort.c
+++ AL/al-midsem-practice/mergesort.c
@@ -1,22 +1,29 @@
 // performing merge sort on an array
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void merge(int *b, int *c, int *a, int n) {
+void merge(int *b, int *c, int *a, int n)
+{
     int i = 0, j = 0, k = 0;
     int p = n / 2, q = n / 2;
-    while (i<p && j<q) {
+    while (i < p && j < q)
+    {
         if (b[i] <= c[j])
             a[k++] = b[i++];
         else if (c[j] < b[i])
             a[k++] = c[j++];
     }
-    if (i==p) {
-        while(j==q) {
+    if (i == p)
+    {
+        while (j == q)
+        {
             a[k++] = c[j++];
         }
-    } else {
-        while (i==p) {
+    }
+    else
+    {
+        while (i == p)
+        {
             a[k++] = b[i++];
         }
     }
@@ -24,16 +31,19 @@ void merge(int *b, int *c, int *a, int n) {
 
 void mergesort(int *a, int n)
 {
-    if (n>1) {
+    if (n > 1)
+    {
         int *b = malloc((n / 2) * sizeof(int));
         int *c = malloc((n / 2) * sizeof(int));
         int i = 0;
-        while (i <= (n/2)-1) {
+        while (i <= (n / 2) - 1)
+        {
             b[i] = a[i];
             ++i;
         }
         int j = 0;
-        while(i <= n-1) {
+        while (i <= n - 1)
+        {
             c[j] = a[i];
             ++i;
             ++j;
@@ -43,15 +53,16 @@ void mergesort(int *a, int n)
         mergesort(c, n / 2);
         merge(b, c, a, n);
 
-        for (int f = 0; f < (n/2); ++f)
+        for (int f = 0; f < (n / 2); ++f)
             printf("%d ", b[f]);
         printf("\nIn mergesort\n");
-        for (int f = 0; f < (n/2)+1; ++f)
+        for (int f = 0; f < (n / 2) + 1; ++f)
             printf("%d ", c[f]);
     }
 }
 
-int main() {
+int main()
+{
     int arr[] = {2, 18, 23, 9, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < n; ++i)
