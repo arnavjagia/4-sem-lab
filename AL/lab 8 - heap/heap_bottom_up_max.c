e721f365eda589c6cc34b9d63433c7616381cd2e
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-03-05 15:32:48 +0530

    Update heap_bottom_up_max.c

--- AL/lab 8 - heap/heap_bottom_up_max.c
+++ AL/lab 8 - heap/heap_bottom_up_max.c
@@ -4,23 +4,22 @@ construction and analyze its time efficiency. Prove experimentally that the wors
 case time complexity is O(n log n)
 */
 
-
 #include <stdio.h>
-#include <stdlib.h>
 
-int op = 0;
+int count = 0;
 
-void heapify(int h[], int l, int n) {
+void heapify(int h[], int n) {
     int i, k, v, heapify, j;
-    for(i = (n/2); i>=l; i--) {
-        k = i; v = h[k]; heapify = 0;
-        while(heapify == 0 && 2*k <= n) {
-            j = 2*k;
-            op++;
-            if(j<n)
-                if(h[j]<h[j+1])
-                    j = j+1;
-            if(v>=h[j])
+    for (i = (n / 2); i >= 1; i--) {
+        k = i;
+        v = h[k];
+        heapify = 0;
+        while (heapify == 0 && 2 * k <= n) {
+            count += 1;
+            j = 2 * k;
+            if (j < n)
+                if (h[j] < h[j + 1]) j = j + 1;
+            if (v >= h[j])
                 heapify = 1;
             else {
                 h[k] = h[j];
@@ -32,29 +31,31 @@ void heapify(int h[], int l, int n) {
     return;
 }
 
-void HeapSort(int arr[], int n) {
-    int k = 0;
-    for(int i = 1; i<=n; i++) {
-        heapify(arr, 1, n - k);
-        int temp = arr[1];
-        arr[1] = arr[n-k];
-        arr[n-k] = temp;
-        k++;
-    }
-}
-
 int main() {
-    int arr[20], n;
-    printf("enter n: ");
+    int h[20], i, n;
+    printf("\nEnter the number of Elements:");
     scanf("%d", &n);
-    printf("enter elements: \n");
-    for(int i = 1; i<=n; i++)
-        scanf("%d", &arr[i]);
-    HeapSort(arr, n);
-    printf("sorted array: \n");
-    for(int i = 1; i<=n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-    printf("Count = %d\n", op);
+    printf("\nEnter the Elements:");
+    for (i = 1; i <= n; i++)
+        scanf("%d", &h[i]);
+    printf("\ndisplay the array: ");
+    for (i = 1; i <= n; i++) {
+        printf("\t%d", h[i]);
+    }
+    heapify(h, n);
+    printf("\nThe heap created: ");
+    for (i = 1; i <= n; i++) {
+        printf("\t%d", h[i]);
+    }
+    printf("\nopcount: %d", count);
+
     return 0;
 }
+
+/* ---SAMPLE IO---
+Enter the number of Elements: 5
+Enter the Elements: 2 3 1 5 4
+display the array: 	2	3	1	5	4
+The heap created: 	5	4	1	3	2
+opcount: 3
+*/
